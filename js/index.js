13355517ebd1f6dc8dc93c9418d050a63d54a2ae
theecn <310659947@QQ.com>
2018-07-09 21:23:47 +0800

    adds

--- js/index.js
+++ js/index.js
@@ -1,10 +1,20 @@
-var xhl = new XMLHttpRequest()
-xhl.open('get','../music.json',true)
-xhl.onload = function() {
-	if ((xhl.status >= 200 &&xhl.status < 300)||xhl.status ===304) {
-		console.log(JSON.parse(this.responseText))
-	} else {
-		console.log('chuchuo')
+getMusicList(function(list){
+	console.log(Array.isArray(list));
+	console.log(list)
+	var lists = list;
+	var song = lists[0];
+	var audio = new Audio(song.src);
+	 audio.play()
+})
+function getMusicList(callback) {
+	var xhl = new XMLHttpRequest()
+	xhl.open('get','../music.json',true)
+	xhl.onload = function() {
+		if ((xhl.status >= 200 &&xhl.status < 300)||xhl.status ===304) {
+			callback(JSON.parse(this.responseText))
+		} else {
+			console.log('helllo')
+		}
 	}
+	xhl.send()
 }
-xhl.send()
